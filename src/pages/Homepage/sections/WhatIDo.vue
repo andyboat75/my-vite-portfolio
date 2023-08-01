b64b6a267b836fcf08d0af1ad5496342c3c220fb
Cybandy <andyboat75@gmail.com>
2023-08-01 04:17:11 +0200

    fixed annoying typescript error

--- src/pages/Homepage/sections/WhatIDo.vue
+++ src/pages/Homepage/sections/WhatIDo.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import CyTabs from '../../../components/CyTabs.vue';
+import { ui as d_ui } from '../../../config';
+// import { Dict } from '../../../types';
+const ui = {...d_ui.value.tabs.list} as any
+delete ui.width
 const items = [{
   label: 'Cybersecurity',
   slot: 'cybersecurity'
@@ -19,7 +23,7 @@ const orientation = computed(() => {
   <div>
     <CyTabs :items="items" :orientation="orientation"
       :ui="{
-        wrapper: orientation == 'vertical' ? 'flex items-center gap-4' : 'relative space-y-2', list: { width: orientation == 'vertical' ? 'w-48' : 'w-full' }
+        wrapper: orientation == 'vertical' ? 'flex items-center gap-4' : 'relative space-y-2', list: {...ui,width: orientation == 'vertical' ? 'w-48' : 'w-full' }
       }">
     <template #cybersecurity="{item}">
      {{ item.label }}
